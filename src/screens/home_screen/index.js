94d51a45f0a55887baa5e5993ddb4573cde58d67
Sourabh <sourabh.khera@tothenew.com>
2019-01-23 02:27:46 +0530

    home screen design done

--- src/screens/home_screen/index.js
+++ src/screens/home_screen/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { getImageWidthAndHeight, getPixelRatio } from '../../utils/commonFuncions';
+import styles from './style';
 
 class HomeScreen extends Component {
   render() {
+    const ratio = getPixelRatio();
+    const profileImageAttributes = getImageWidthAndHeight(50, 50);
+    const postImageAttributes = getImageWidthAndHeight(0, 250);  
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+      <View style={styles.homeScreenContainer}>
+        <View style={styles.thumbnailContainer}>
+           <View style={{...styles.thumbNail, height: profileImageAttributes.height / ratio, width: profileImageAttributes.width / ratio}}/>
+           <Text style={styles.nameText}>Sourabh Khera</Text>
+        </View>
+        <View style={{width: '100%', height: postImageAttributes.height / ratio, backgroundColor: 'gray'}} />
+        <View style={styles.likeContainer}>
+          <Ionicons name="md-heart-empty" size={28} />
+          <Text style={styles.dateTimeText}>14th Sep, 11:00AM</Text>
+        </View>
       </View>
     );
   }
